7b2d7f5e1571e32d6e6098d8889813daefcda227
Alan Ribeiro <alanribeiro@MacBook-de-Alan.local>
2017-09-20 12:33:37 -0300

    Configure Babel to transpile React and webpack to import css file

---
+++ exercicios_webpack/ex.4/index.js
@@ -0,0 +1,8 @@
+import 'react'
+import './estilo.css'
+
+export default props => (
+	<h1>Olá</h1>
+)
+
+console.log('Funcionou!')
--- exercicios_webpack/webpack.config.js
+++ exercicios_webpack/webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
  module.exports = {
- 	entry: './ex.3/index.js',
+ 	entry: './ex.4/index.js',
  	output: {
  		path: __dirname + '/public',
  		filename: './bundle.js'
@@ -10,15 +11,21 @@ const webpack = require('webpack');
  		port: 8080,
  		contentBase: './public'
  	},
+ 	plugins: [
+ 		new ExtractTextPlugin('app.css')
+ 	],
  	module: {
  		loaders: [{
  			test: /.js?$/,
  			loader: 'babel-loader',
  			exclude: /node_modules/,
  			query: {
- 				presets: ['es2015'],
+ 				presets: ['es2015','react'],
  				plugins: ['transform-object-rest-spread']
- 			}
+ 			} 			
+ 		},{
+ 			test: /\.css$/,
+ 			loader: ExtractTextPlugin.extract("style-loader","css-loader")
  		}]
  	}
  }
